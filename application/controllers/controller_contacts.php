3322fad955c8ab4b7bdbab4229ab9faaa217fe75
kirill <kirill@kirill-S300CA>
2014-08-31 19:10:18 +0400

    Updates

--- application/controllers/controller_contacts.php
+++ application/controllers/controller_contacts.php
@@ -6,6 +6,7 @@ class Controller_Contacts extends Controller
 	function action_index()
 	{
 		session_start();
+		
 		print_r($_SESSION);
 		$this->view->generate('contacts_view.php', 'template_view.php');
 	}
